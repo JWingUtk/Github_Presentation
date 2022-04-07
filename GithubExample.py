481c786ab4837ad1346272c916c6cbd3179e4888
Jonathan <97312558+JWingUtk@users.noreply.github.com>
2022-04-07 13:44:26 -0400

    Added a first piece of example code
    It sums an array

---
+++ GithubExample.py
@@ -0,0 +1,24 @@
+# Example for Dr. Sobes Presentation on Github for pull requests
+
+import time as t
+import numpy as np
+
+Tock = t.time()
+
+Array = np.random.rand(10000000)
+
+Sum = 0
+for i in range(len(Array)):
+    Sum = Sum + Array[i]
+
+Tick = t.time()
+Time = Tick-Tock
+
+print ("This process took "+str(Time)+" seconds")
+
+# Sum2 = Array.sum()
+
+# Tick2 = t.time()
+# Time2=Tick2-Tick
+# print("But this one only takes "+str(Time2)+" seconds")
+# print("That's "+str(Time/Time2)+" times faster!!")
